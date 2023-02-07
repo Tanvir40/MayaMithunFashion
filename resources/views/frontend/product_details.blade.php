63fa680f2e05a7d4d3a51183ab2a15fa32b0f8ea
Abdus Samad <120012678+shakziaurrahmantitoofficial@users.noreply.github.com>
2023-02-07 15:37:23 +0600

    update

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -1,11 +1,58 @@
 @include('../frontend/inc/header')
 
-  <br><br><br>
+  <section class="section my-5 py-5">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-md-6 col-12 text-center">
+            <img src="{{asset('Image/Untitled-design-2022-03-05T171515.074.png')}}" class="img-fluid" width="70%">
+          </div>
+
+          <div class="col-md-6 col-12">
+            <h4>Men’s</h4>
+            <p>100% Cotton Frence Terry Sweat Shirt 220 Gsm</p>
+            <span class="posted_in">Categories: <a href="" rel="tag">Knitwear Item</a>, <a href="" rel="tag">Men</a></span>
+          </div>
+
+          <div class="col-12">
+              <h6 class="mb-4" style="font-weight: bold;border-top: 3px solid #50bb26;padding-top: 3px;display: inline-block;">Description</h6>
+              <p>100% Cotton F.Terry Sweat Shirt with Neck, Cuff, Bottom Rib.</p>
+              <p>Weight 220 gsm.</p>
+              <p>Body Length 70 Cm, Sleeve Length 54, Chest 51 Cm</p>
+              <h4 style="font-weight: bold;">Related products</h4>
+              
+              <div class="row">
+
+                <div class="col-md-4 col-12 text-center">
+                  <img src="https://nankarfashion.com/wp-content/uploads/2022/03/Untitled-design-82.png" class="img-fluid" width="60%">
+                  <h4 style="text-align: left;">Men</h4>
+                  <a style="text-align: left;" href="" class=""><h2 style="font-size: 18px;">Sweater</h2></a>
+                  <p style="text-align: left;">Star</p>
+                </div>
 
 
-  <h1>This is product deatails section</h1>
+                <div class="col-md-4 col-12 text-center">
+                  <img src="https://nankarfashion.com/wp-content/uploads/2022/03/Untitled-design-82.png" class="img-fluid" width="60%">
+                  <h4 style="text-align: left;">Men</h4>
+                  <a style="text-align: left;" href="" class=""><h2 style="font-size: 18px;">Sweater</h2></a>
+                  <p style="text-align: left;">Star</p>
+                </div>
+
+                <div class="col-md-4 col-12 text-center">
+                  <img src="https://nankarfashion.com/wp-content/uploads/2022/03/Untitled-design-82.png" class="img-fluid" width="60%">
+                  <h4 style="text-align: left;">Men</h4>
+                  <a style="text-align: left;" href="" class=""><h2 style="font-size: 18px;">Sweater</h2></a>
+                  <p style="text-align: left;">Star</p>
+                </div>
+
+              </div>
 
 
-  <br><br><br>
+          </div>
 
-  @include('../frontend/inc/footer')
+        </div>
+      </div>
+  </section>
+
+
+@include('../frontend/inc/footer')
